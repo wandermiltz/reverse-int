96e7d82ee845bad320673c5abde1ae2d85d6c046
Olga Berezka <olgakoltsova@Air-Olga.lan>
2023-01-14 19:30:56 +0000

    feat: add reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+
+	const str = n.toString()
+	let strReversed
+
+	if (str[0] === '-') {
+		strReversed = str.substring(1).split("").reverse().join("");
+	} else {
+		strReversed = str.split("").reverse().join("");
+	}
+	return Number(strReversed)
 }
